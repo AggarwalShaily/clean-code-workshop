7c98cd7d0e54d9c91b0a54081fbe23a11c6e6d85
Shaily Aggarwal <in-shaily.aggarwal@Shailys-MacBook-Pro.local>
2019-08-27 14:06:10 +0530

    Testing commit

--- src/main/java/com/movierental/Rental.java
+++ src/main/java/com/movierental/Rental.java
@@ -17,4 +17,41 @@ public class Rental {
   public Movie getMovie() {
     return movie;
   }
+
+  double amount() {
+    double amount = 0;
+
+    switch (movie.getPriceCode()) {
+      case Movie.REGULAR:
+        amount += 2;
+        if (daysRented > 2)
+          amount += (daysRented - 2) * 1.5;
+        break;
+
+      case Movie.NEW_RELEASE:
+        amount += daysRented * 3;
+        break;
+
+      case Movie.CHILDRENS:
+        amount += 1.5;
+        if (daysRented > 3)
+          amount += (daysRented - 3) * 1.5;
+        break;
+    }
+
+    return amount;
+  }
+
+  int frequentRenterPoints() {
+    int frequentRenterPoints = 1;
+
+    if (isBonusApplicable())
+      frequentRenterPoints++;
+
+    return frequentRenterPoints;
+  }
+
+  private boolean isBonusApplicable() {
+    return (movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1;
+  }
 }
